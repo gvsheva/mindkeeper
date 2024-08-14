dc543546cc3f8b36b1b31e91ad8b1e4fc5ddec07
gvsheva 3405066+gvsheva@users.noreply.github.com <g.shevchenko@infingame.com>
2024-08-14 19:16:42 +0300

    set argument parser defaults

--- mindkeeper/parser.py
+++ mindkeeper/parser.py
@@ -8,8 +8,14 @@ class CommandArgumentError(ValueError):
 
 
 class CommandArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, name):
+        super().__init__(
+            name,
+            prefix_chars="/",
+            add_help=False,
+            allow_abbrev=False,
+            exit_on_error=False,
+        )
         self.known_args = set()
 
     def add_argument(self, *args, **kwargs):
