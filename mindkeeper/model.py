15d91b43e58f5f53e05654bab5186c4017d9d5e5
gvsheva 3405066+gvsheva@users.noreply.github.com <g.shevchenko@infingame.com>
2024-08-14 11:51:46 +0300

    suggestions

--- mindkeeper/model.py
+++ mindkeeper/model.py
@@ -1,31 +1,37 @@
+from datetime import datetime
+from enum import StrEnum, auto
 from typing import Annotated
 from uuid import UUID, uuid4
 
 import annotated_types as at
-from pydantic import BaseModel, Field, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, Field
+
+Tags = list[Annotated[str, at.Len(1, 100)]]
+
 
 class Note(BaseModel):
     id: UUID = Field(..., default_factory=uuid4)
     title: Annotated[str, at.Len(1, 100)]
     text: Annotated[str, at.Len(1, 1000)]
-    tags: list[Annotated[str, at.Len(1, 100)]
-               ] = Field(..., default_factory=list)
-    
-class PhoneNumber(BaseModel):
-    number: str = Field(...)
+    tags: Tags = Field(..., default_factory=list)
 
-    @field_validator('number')
-    def validate_phone_number(cls, value):
-        import re
-        pattern = re.compile(r'^\+?\d{10,15}$')
-        if not pattern.match(value):
-            raise ValueError('Invalid phone number format')
-        return value
-    
+
+class PhoneType(StrEnum):
+    MOBILE = auto()
+    WORK = auto()
+    HOME = auto()
+
+
+class Phone(BaseModel):
+    number: str = Field(..., pattern=r'^\+?\d{10,15}$')
+    type: PhoneType = PhoneType.MOBILE
+
+
 class Contact(BaseModel):
     id: UUID = Field(..., default_factory=uuid4)
     name: Annotated[str, at.Len(1, 100)]
-    address: Annotated[str, at.Len(1, 250)]
-    phone_number: PhoneNumber
-    email: EmailStr
-    birthday: Annotated[str, at.Len(10)]  # format YYYY-MM-DD
+    address: Annotated[str, at.Len(1, 250)] | None = None
+    email: EmailStr | None = None
+    phones: list[Phone] = Field(..., default_factory=list)
+    birthday: datetime | None = None
+    tags: Tags = Field(..., default_factory=list)
