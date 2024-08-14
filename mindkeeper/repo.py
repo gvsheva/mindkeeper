f661c098ea6e7ea140db0c37f88dbe8f316f9994
gvsheva 3405066+gvsheva@users.noreply.github.com <g.shevchenko@infingame.com>
2024-08-14 21:32:23 +0300

    base repo implementation

--- mindkeeper/repo.py
+++ mindkeeper/repo.py
@@ -1,20 +1,43 @@
-from uuid import UUID
+from enum import StrEnum, auto
 
-from mindkeeper.model import Note
+from mindkeeper.model import GENERATE, Note
+
+
+class _INDEXES(StrEnum):
+    NOTES = auto()
+    CONTACTS = auto()
 
 
 class Repo:
-    def put_note(self, note: Note):
-        print(f"Putting note: {note}")
+    def __init__(self):
+        self.data = {}
 
-    def get_note(self, id: UUID):
-        ...
+    def _generate_id(self, counter_name: str):
+        counter = self.data.get(counter_name, 0)
+        counter += 1
+        self.data[counter_name] = counter
+        return counter
+
+    def put_note(self, note: Note):
+        if note.id is GENERATE:
+            note.id = self._generate_id("__notes_counter")
+        print(f"Adding note: {note}")
+        index = self.data.get(_INDEXES.NOTES, {})
+        index[note.id] = note
+        self.data[_INDEXES.NOTES] = index
+
+    def get_note(self, id: int) -> Note:
+        index = self.data.get(_INDEXES.NOTES, {})
+        return index.get(id)
 
     def find_notes(self, query: str):
         ...
 
-    def delete_note(self, id: UUID):
-        ...
+    def delete_note(self, id: int):
+        index = self.data.get(_INDEXES.NOTES, {})
+        del index[id]
+        self.data[_INDEXES.NOTES] = index
 
     def wipe_notes(self):
-        ...
+        self.data[_INDEXES.NOTES] = {}
+        self.data["__notes_counter"] = 0
